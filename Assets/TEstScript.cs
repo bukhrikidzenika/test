02352546e277dade35c8e6d2c8c5b34945659fc0
nikoloz bukhrikidze <bukhrikidze@gmail.com>
2022-07-10 14:22:29 +0400

    add test

---
+++ Assets/TEstScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TEstScript : MonoBehaviour
+{   int a =1;
+int b =99999;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    { print(a+b);
+}
+}
